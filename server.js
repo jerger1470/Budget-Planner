1219a7370a4174f1ad66877a62fe3216547e46a1
Bryan Jerger <Bjerger1470@gmail.com>
2020-03-05 21:44:29 -0600

    spelling error

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://localhost/budgetOffline",
+  process.env.MONGODB_URL || "mongodb://localhost/budgetOffline",
 {
   useNewUrlParser: true,
   useFindAndModify: false,
