688c1e0e1d9acb68e0117e1c37cfde9b66c15e80
Bryan Jerger <Bjerger1470@gmail.com>
2020-03-07 09:51:50 -0600

    add cors

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
+const cors = require("cors");
 
-const PORT = 3000;
+const PORT = process.env.PORT||3000;
 
 const app = express();
 require("dotenv/config");
